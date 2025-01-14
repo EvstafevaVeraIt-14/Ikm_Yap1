c553e2c9568f5af86357c8cbec87d2d822f5664d
EvstafevaVeraIt-14 <evstafevavera28@gmail.com>
2025-01-15 02:05:12 +0500

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+bool proverka(const string& str);
+int izstrvint(const string& str);
+void schitalka(int a[], int& size);
+void umenshalka(int n, int a[], int& size);
+void vyvod(int a[], int size);
